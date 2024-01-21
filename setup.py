6b0d64d53dc0ce4c4652b581e0908c60912b6cb6
Durga Prasad <saikamireddi3@gmail.com>
2024-01-21 11:47:43 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
-from setuptools import find_packages,setup
-from typing import List
+import setuptools
 
-HYPEN_E_DOT='-e .' # It is used to trigger the setup.py
-def get_requirements(file_path:str)->List[str]:
-    'this function will returns the list of requirements'
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace('\n','') for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+__version__ = "0.0.0"
 
-setup(
+REPO_NAME = "chicken_disease_classification-n"
+AUTHOR_USER_NAME = "saidurga4521"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "saikamireddi3@gamil.com"
 
- name='cnn_classification',
- version='0.0.1',
- author='sai',
- author_email='saikamireddi3@gmail.com',
- packages=find_packages(),
- install_requires=get_requirements('requirements.txt')
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
